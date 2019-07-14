9909dde7a88d20851ff65c98fb421521ed6414dc
a.kapustkin <a.kapustkin@gmail.com>
2019-07-14 21:42:21 +0300

    Lesson_11_Initial

---
+++ Lesson_11/src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"../src/internal"
+)
+
+func main() {
+	argFrom := flag.String("from", "", "Путь к исходному файлу")
+	argTo := flag.String("to", "", "Путь к новому файлу")
+	argBuff := flag.Int("buffer", 1024, "Размер буфера, в кБ")
+	flag.Parse()
+	err := internal.CopyFileToFile(*argFrom, *argTo, *argBuff)
+	if err != nil {
+		log.Fatalf("Ошибка при выполнении операции! %s", err)
+	}
+}
