f3973f5899a687a61d1e58bbd1d48916726fdf52
a.kapustkin <a.kapustkin@gmail.com>
2019-07-15 21:35:14 +0300

    fix

--- Lesson_11/src/go.mod
+++
@@ -1,5 +0,0 @@
-module mod
-
-go 1.12
-
-require github.com/kapustkin/GoHomeWork v0.0.0-20190714192153-a7e70569e578
--- Lesson_11/src/internal/progressBar.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-import (
-	"github.com/cheggaaa/pb/v3"
-)
-
-func progress() {
-	bar := pb.StartNew(count)
-}
